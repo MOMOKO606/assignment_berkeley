06195a386812b540c4fc1e3e37f4a976a296afa4
BianLong0618 <bianlong0618@qq.com>
2024-10-12 10:05:59 -0400

    Created a simple version of db and related db classes

---
+++ assignment_berkeley/db/models.py
@@ -0,0 +1,101 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Numeric,
+    DateTime,
+    Enum,
+    ForeignKey,
+    Table,
+)
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+import uuid
+import enum
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+
+
+class DBCustomer(Base):
+    __tablename__ = "customer"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(250), nullable=True)
+    last_name = Column(String(250), nullable=True)
+    email = Column(String(250), nullable=True)
+
+
+class OrderStatus(enum.Enum):
+    pending = "pending"
+    canceled = "canceled"
+    completed = "completed"
+
+
+class PaymentStatus(enum.Enum):
+    unpaid = "unpaid"
+    paid = "paid"
+    failed = "failed"
+
+
+class DBProduct(Base):
+    __tablename__ = "product"
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
+    name = Column(String(250), nullable=False)
+    description = Column(String(500), nullable=True)
+    price = Column(Numeric(10, 2), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
+
+
+order_product = Table(
+    "order_product",
+    Base.metadata,
+    Column("order_id", UUID(as_uuid=True), ForeignKey("orders.id"), primary_key=True),
+    Column(
+        "product_id", UUID(as_uuid=True), ForeignKey("product.id"), primary_key=True
+    ),
+    Column("quantity", Integer, nullable=False),
+)
+
+
+class DBOrder(Base):
+    __tablename__ = "orders"
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
+    customer_id = Column(Integer, ForeignKey("customer.id"), nullable=False)
+    total_price = Column(Numeric(10, 2), nullable=False)
+    status = Column(Enum(OrderStatus), nullable=False, default=OrderStatus.pending)
+    payment_status = Column(
+        Enum(PaymentStatus), nullable=False, default=PaymentStatus.unpaid
+    )
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
+
+    customer = relationship(
+        "DBCustomer", backref=backref("orders", cascade="all, delete-orphan")
+    )
+    products = relationship(
+        "DBProduct", secondary=order_product, back_populates="orders"
+    )
+
+
+DBProduct.orders = relationship(
+    "DBOrder", secondary=order_product, back_populates="products"
+)
--- assignment_berkeley/main.py
+++ assignment_berkeley/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from assignment_berkeley.db.engine import DBSession, init_db
+from assignment_berkeley.db.models import DBCustomer
 
 app = FastAPI()
+
+DB_FILE = "sqlite:///berkeley.db"
+
+
+@app.on_event("startup")
+def startup_event():
+    init_db(DB_FILE)
+
+
+@app.get("/customers")
+def read_all_customers():
+    session = DBSession()
+    customers = session.query(DBCustomer).all()
+    return customers
 
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
